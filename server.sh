e1d511dbc4ddee6afd4ef11800014beb7854d3a7
LorenzoSiena <74120782+LorenzoSiena@users.noreply.github.com>
2021-11-13 10:53:11 +0100

    Update server.sh

--- server.sh
+++ server.sh
@@ -12,30 +12,21 @@ echo  "     1->Start&Run"
 echo  "     2->Start MySqlServer"
 echo  "     3->Stop MySqlServer"
 echo  "     4->Esegui MySql_Update"
-echo  "Esegui :"
+echo   -n "Esegui :"
 read CMD
 trap do_for_sigint 2
 case $CMD in
   1)	sudo /opt/lampp/lampp startmysql && sudo /opt/lampp/manager-linux-x64.run 
-		
-;;
-
+	;;
   2)	sudo /opt/lampp/lampp startmysql
-	
-;;
-
+	;;
   3)	sudo /opt/lampp/lampp stopmysql	
-	
-;;
+	;;
   4)	sudo /opt/lampp/bin/mysql_upgrade 	
-	
-;;
-
-*)
-
-echo “Scelta CMD non valida”
-
-exit 1
-
+	;;	
+  *)
+ 	echo “Scelta CMD non valida”
+ 	sleep 1.5s
+	exit 0
+	;;
 esac
-
